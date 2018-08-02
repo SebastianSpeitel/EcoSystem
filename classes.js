631363d6ca9363782dfe4efb1b9cc524216dfca4
Sebastian Speitel <sebastian.speitel@outlook.de>
2018-08-02 18:12:15 +0200

    added a lot of stuff

--- classes.js
+++ classes.js
@@ -3,7 +3,9 @@
 
 class LifeForm {
     constructor() {
-        this.pos = { x: 0, y: 0 };
+        this.pos = createVector();
+        this.health = 0;
+        this.speed = 10;
     }
 
     set r(r) {
@@ -16,13 +18,32 @@ class LifeForm {
 
     show() {
         noStroke();
-        fill(255);
-        ellipse(this.pos.x, this.pos.y, this.r);
+        fill(this.color || 255);
+        ellipse(this.pos.x, this.pos.y, this.r * 2);
     }
-    
+
+    tick(dt = 1) {
+        if (this.vel) {
+            this.vel.add(createVector(random(-5, 5), random(-5, 5)));
+            this.pos.add(this.vel.copy().limit(this.speed).mult(dt));
+        }
+        this.health -= 0.01 * dt;
+        if (this.health < 1) this.die();
+    }
+
+    eat(food) {
+        this.health += food.health;
+        food.die();
+    }
+
+    die() {
+        lifeForms.delete(this);
+    }
+
+
 }
 
-class Plant extends LifeForm{
+class Plant extends LifeForm {
     constructor() {
         super();
         do {
@@ -30,8 +51,7 @@ class Plant extends LifeForm{
             this.nut = floor(random(5, 10));
         } while (this.r < 10);
 
-        this.pos.x = random(this.r, width - this.r);
-        this.pos.y = random(this.r, height - this.r);
+        this.pos.set(random(this.r, width - this.r), random(this.r, height - this.r));
     }
 
     get r() {
@@ -42,11 +62,92 @@ class Plant extends LifeForm{
         noStroke();
         //outer circle (nut)
         fill(nutcolor);
-        ellipse(this.pos.x, this.pos.y, this.r);
+        ellipse(this.pos.x, this.pos.y, this.r * 2);
         //inner circle (plant)
         fill(plantcolor);
-        ellipse(this.pos.x, this.pos.y, this.health);
+        ellipse(this.pos.x, this.pos.y, this.health * 2);
     }
 
+}
+
+class Herbivore extends LifeForm {
+    constructor(health) {
+        super();
+        this.health = health || floor(random(5, 10));
+
+        this.pos.set(random(this.r, width - this.r), random(this.r, height - this.r));
+
+        this.speed = 25;
+        this.color = herbivorecolor;
+        this.vel = createVector();
+    }
+
+    get r() {
+        return this.health;
+    }
+
+    tick(dt = 1) {
+        let target = findClosest(this.pos, Plant);
+        if (target) {
+            let rel = target.pos.copy().sub(this.pos);
+            if (rel.mag() < this.r + target.r) this.eat(target);
+            this.vel.lerp(rel, 0.9 * dt);
+        }
+        if (this.health > 20) this.split();
+        super.tick(dt);
+    }
+
+    split() {
+        this.health *= 0.5;
+        let twin = new Herbivore(this.health);
+        twin.pos = this.pos.copy();
+        this.vel.x = -10;
+        twin.vel.x = 10;
+        //twin.vel.add(createVector(random(-15, 15), random(-15, 15)));
+        lifeForms.add(twin);
+    }
+}
+
+class Carnivore extends LifeForm {
+    constructor(health) {
+        super();
+        this.health = health || floor(random(5, 10));
+
+        this.pos.x = random(this.r, width - this.r);
+        this.pos.y = random(this.r, height - this.r);
+
+        this.speed = 10;
+        this.color = carnivorecolor;
+        this.vel = createVector();
+    }
+
+    get r() {
+        return this.health;
+    }
+
+    tick(dt = 1) {
+        let target = findClosest(this.pos, Herbivore);
+        if (target) {
+            let rel = target.pos.copy().sub(this.pos);
+            if (rel.mag() < this.r + target.r) this.eat(target);
+            this.vel.lerp(rel, 0.9 * dt);
+        }
+        if (this.health > 30) this.split();
+
+        this.health *= 0.999;
+        this.health -= 0.1 * dt;
+        super.tick(dt);
+    }
+
+    split() {
+        this.health *= 0.2;
+        let twin = new Carnivore(this.health);
+        twin.pos = this.pos.copy();
+        this.vel.x = -10;
+        twin.vel.x = 10;
+        //twin.pos.add(createVector(random(2, 3), random(2, 3)).mult(random(-15, 15)));
+        //console.log(twin);
+        lifeForms.add(twin);
+    }
 }
 
--- sketch.js
+++ sketch.js
@@ -1,19 +1,25 @@
 let w = 1024
 let h = 576
 let popSize = 20;
-let lifeForms = [];
+let lifeForms = new Set();
 let plantcolor;
 let nutcolor;
+let herbivorecolor;
+let carnivorecolor;
 
 function setup() {
     plantcolor = color(0, 255, 0);
     nutcolor = color(139, 69, 19);
+    herbivorecolor = color(0, 0, 255);
+    carnivorecolor = color(255, 0, 0);
 
     let canvas = createCanvas(w, h);
     canvas.parent('sketch-holder');
 
     for (i = 0; i < popSize; i++) {
-        lifeForms.push(new Plant());
+        lifeForms.add(new Plant());
+        lifeForms.add(new Carnivore());
+        lifeForms.add(new Herbivore());
     }
 }
 
@@ -21,6 +27,16 @@ function draw() {
 
     background(0, 0, 0, 255);
 
-    lifeForms.forEach(l => l.show());
+    let fps = frameRate() || 60;
+    lifeForms.forEach(l => l.tick(1 / fps) || l.show());
 
+    if (lifeForms.size < 100) lifeForms.add(new Plant());
+}
+
+function findClosest(pos, cls) {
+    let closest = null;
+    lifeForms.forEach(l => {
+        if (l instanceof cls && (!closest || l.pos.dist(pos) < closest.pos.dist(pos))) closest = l;
+    });
+    return closest;
 }
